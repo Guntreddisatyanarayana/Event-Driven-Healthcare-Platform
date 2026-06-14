bc5d255ae944408267fe03fb33018feb22255daf
satya <satyaguntreddi8@gmail.com>
2026-06-14 22:09:50 +0530

    Add email uniqueness validation and business logic

--- patient-service/src/main/java/com/satya/healthcare/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/satya/healthcare/patientservice/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.satya.healthcare.patientservice.mapper;
 
+import com.satya.healthcare.patientservice.dto.PatientRequestDto;
 import com.satya.healthcare.patientservice.dto.PatientsResponseDto;
 import com.satya.healthcare.patientservice.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientsResponseDto toDto(Patient patient){
@@ -13,5 +16,14 @@ public class PatientMapper {
         patientDto.setDob(patient.getDob().toString());
         return  patientDto;
 
+    }
+    public static Patient toModel(PatientRequestDto patientRequestDto){
+        Patient p1=new Patient();
+        p1.setName(patientRequestDto.getName());
+        p1.setEmail(patientRequestDto.getEmail());
+        p1.setAddress(patientRequestDto.getAddress());
+        p1.setDob(LocalDate.parse(patientRequestDto.getDob()));
+        p1.setRegistrationDate(LocalDate.parse(patientRequestDto.getRegistrationDate()));
+ return p1;
     }
 }
--- patient-service/src/main/java/com/satya/healthcare/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/satya/healthcare/patientservice/service/PatientService.java
@@ -1,13 +1,18 @@
 
 package com.satya.healthcare.patientservice.service;
 
+import com.satya.healthcare.patientservice.dto.PatientRequestDto;
+import com.satya.healthcare.patientservice.exception.EmailAlreadyExistException;
+import com.satya.healthcare.patientservice.exception.PatientNotFoundException;
 import com.satya.healthcare.patientservice.mapper.PatientMapper;
 import com.satya.healthcare.patientservice.model.Patient;
 import com.satya.healthcare.patientservice.dto.PatientsResponseDto;
 import com.satya.healthcare.patientservice.repositery.PatientRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PatientService {
@@ -15,9 +20,42 @@ public class PatientService {
     public PatientService(PatientRepository patientRepoistery){
         this.patientRepoistery=patientRepoistery;
     }
-    /// return the data to client through dto(because privacy)
+    /// return the data to client through dto(because of privacy)
     public List<PatientsResponseDto> getPatients(){
-        List<Patient> patients=patientRepoistery.findAll();//to find all patient entity objects
-        return patients.stream().map(PatientMapper::toDto).toList();//returninh lost of dtos to controller
+
+        List<Patient> patients=patientRepoistery.findAll();///to find all patient
+        return patients.stream().map(PatientMapper::toDto).toList();///return a list of patients
     }
+    /// // create the patient
+    public  PatientsResponseDto createPatients(PatientRequestDto patientRequestDto){
+        if(patientRepoistery.existsByEmail(patientRequestDto.getEmail()))
+        {
+            throw  new EmailAlreadyExistException("A Patient Already Exist this email"+patientRequestDto.getEmail());
+        }
+        Patient newPatient= patientRepoistery.save(PatientMapper.toModel(patientRequestDto));///covert dto to model
+
+        return PatientMapper.toDto(newPatient);///convert model  to dto
+    }
+
+    /// / Update the patient
+    public PatientsResponseDto updatePatient(UUID id, PatientRequestDto patientRequestDto){
+        Patient patient=patientRepoistery.findById(id).orElseThrow(()->new PatientNotFoundException("Patient Not Found ID"+id));///if id not found
+
+        if(patientRepoistery.existsByEmail(patientRequestDto.getEmail()))
+        {
+            throw  new EmailAlreadyExistException("A Patient Already Exist this email"+patientRequestDto.getEmail());///if already exist
+        }
+
+        Patient updatePatientModel=PatientMapper.toModel(patientRequestDto);///convert a PatientsResponseDto to updatePatientModel
+        updatePatientModel=patientRepoistery.save(updatePatientModel);
+        return PatientMapper.toDto(updatePatientModel);///convert a updatePatientModel to PatientsResponseDto
+    }
+    /// /Delete Patient
+    public String deletePatient(UUID id){
+        Patient patientFound=patientRepoistery.findById(id).orElseThrow(()->new PatientNotFoundException("Patient Not Found ID"+id));
+        patientRepoistery.deleteById(id);
+        return "delete successfully";
+    }
+
+
 }
